241986eedd1005db7774bac1682a87c94f012c18
pwitko <piotr@landingi.com>
2017-07-31 23:29:20 +0200

    Fix backpress listener to only quit app when on attendees screen

--- src/Shared/Navigation/MainScreen.js
+++ src/Shared/Navigation/MainScreen.js
@@ -1,13 +1,38 @@
 import { DrawerNavigator } from 'react-navigation';
+import { BackHandler } from 'react-native'
 import RegistrationTab from './RegistrationTab';
 import RoundsTab from './RoundTab';
 import StandingsTab from './StandingsTab';
 
-export default MainScreen = DrawerNavigator(
-    {
-        attendees: { screen: RegistrationTab },
-        routes: { screen: RoundsTab },
-        standings: { screen: StandingsTab }
-    },
-    { initialRouteName: 'attendees' }
-);
+const routes = {
+    attendees: { screen: RegistrationTab, name: 'attendees' },
+    routes: { screen: RoundsTab, name: 'routes' },
+    standings: { screen: StandingsTab, name: 'standings' }
+}
+
+const config = {
+    initialRouteName: 'attendees'
+}
+
+export default class MainScreen extends DrawerNavigator(routes, config) {
+    shouldCloseApp(navState) {
+        return navState.index === 0 && navState.routes.index === 0
+    }
+
+    componentDidMount() {
+        this.handler = BackHandler.addEventListener('backPress', () => {
+            const { dispatch, state } = this.props.navigation
+
+            if (this.shouldCloseApp(state)) {
+                return false
+            }
+
+            dispatch({ type: 'Navigation/BACK' })
+            return true
+        })
+    }
+
+    componentWillUnmount() {
+        this.handler.remove()
+    }
+}
