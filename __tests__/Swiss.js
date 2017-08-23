0b502b7bb4b8c3b17f7c090716b70aebd326f9ac
pwitko <piotr@landingi.com>
2017-08-23 20:38:08 +0200

    Mock TouchableNativeFeedback in test

--- __tests__/Swiss.js
+++ __tests__/Swiss.js
@@ -1,11 +1,21 @@
 import 'react-native';
-import React from 'react';
-import Swiss from '../src/Swiss';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import Swiss from '../src/Swiss'
+import renderer from 'react-test-renderer'
+
+jest.mock('TouchableNativeFeedback', () => {
+    const mock = require('react-native/jest/mockComponent')
+    const mocked = mock('TouchableNativeFeedback')
+
+    mocked.Ripple = () => ({ type: 'Ripple' })
+    mocked.canUseNativeForeground = () => false
+
+    return mocked
+})
 
 it('renders correctly', () => {
     renderer.create(
         <Swiss />
     );
-});
+})
 
