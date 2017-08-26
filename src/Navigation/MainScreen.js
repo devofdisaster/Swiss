b0908840161cd4b56812f6817373b63cd7de6f79
pwitko <piotr@landingi.com>
2017-08-26 14:28:33 +0200

    Fix issues with multiple stacked Players screens on app init

--- src/Navigation/MainScreen.js
+++ src/Navigation/MainScreen.js
@@ -15,7 +15,8 @@ const config = { initialRouteName: 'players' }
 
 export default class MainScreen extends DrawerNavigator(routes, config) {
     shouldCloseApp(navState) {
-        return navState.index === 0 && navState.routes.index === 0
+        console.log(navState)
+        return navState.index === 0 && navState.routes[0].index === 0
     }
 
     componentDidMount() {
--- src/Shared/InitialState.js
+++ src/Shared/InitialState.js
@@ -1,7 +1,15 @@
+import { NavigationActions } from 'react-navigation'
 import MainScreen from '../Navigation/MainScreen'
 
+const initialAction = NavigationActions.reset({
+    index: 0,
+    actions: [
+        NavigationActions.navigate({ routeName: 'Players' })
+    ]
+})
+
 export default initialState = {
-    nav: MainScreen.router.getStateForAction(MainScreen.router.getActionForPathAndParams('players')),
+    nav: MainScreen.router.getStateForAction(initialAction),
     players: [],
     newPlayer: {
         firstname: null,
