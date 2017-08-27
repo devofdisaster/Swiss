1c4e36d91815fb4f973b235c8e286053a1e4866f
pwitko <piotr@landingi.com>
2017-08-27 10:58:52 +0200

    Make the PlayerReducer test expectations more explicit

--- __tests__/Players/Reducers/PlayersReducer.js
+++ __tests__/Players/Reducers/PlayersReducer.js
@@ -12,14 +12,20 @@ describe('PlayersReducer', () => {
 
     it('should add a new player on Players/SaveNew', () => {
         const newPlayer = { id: 'player-five-hundred', nickname: 'xXxLegolas9273xXx', score: 9001 }
-        const expectedState = { ...state, [newPlayer.id]: newPlayer }
+        const expectedState = {
+            ...state,
+            'player-five-hundred': newPlayer
+        }
 
         expect(PlayersReducer(state, SaveNewPlayer(newPlayer))).toEqual(expectedState)
     })
 
     it('should update an existing player on Players/SaveExisting', () => {
         const existingPlayer = { id: 'player-one', nickname: 'Bob' }
-        const expectedState = { ...state, [existingPlayer.id]: { ...state[existingPlayer.id], ...existingPlayer } }
+        const expectedState = {
+            'player-one': { id: 'player-one', nickname: 'Bob', score: 37 },
+            'player-two': { id: 'player-two', nickname: 'Martin', score: 56  },
+        }
 
         expect(PlayersReducer(state, SaveExistingPlayer(existingPlayer))).toEqual(expectedState)
     })
