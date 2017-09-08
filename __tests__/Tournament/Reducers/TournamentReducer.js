e176f69b762f69bd4634560ea721b512ff015f24
pwitko <piotr@landingi.com>
2017-09-08 15:01:25 +0200

    Update tournament reducer to handle show/hide modal actions

--- __tests__/Tournament/Reducers/TournamentReducer.js
+++ __tests__/Tournament/Reducers/TournamentReducer.js
@@ -1,12 +1,82 @@
 import InitialState from '../../../src/Shared/InitialState'
 import TournamentReducer from '../../../src/Tournament/Reducers/TournamentReducer'
+import ShowLoadModal from '../../../src/Tournament/ActionCreators/ShowLoadModal'
+import ShowSaveModal from '../../../src/Tournament/ActionCreators/ShowSaveModal'
+import HideLoadModal from '../../../src/Tournament/ActionCreators/HideLoadModal'
+import HideSaveModal from '../../../src/Tournament/ActionCreators/HideSaveModal'
+import HideTournamentModals from '../../../src/Tournament/ActionCreators/HideTournamentModals'
 
 jest.mock('../../../src/Shared/InitialState');
 
 describe('TournamentReducer', () => {
-    const state = InitialState.tournament
+    const state = {
+        name: 'Test Tournament',
+        loading: false,
+        saveModal: false,
+        loadModal: false
+    }
 
-    it('does nothing currently', () => {
-        expect(TournamentReducer(state, { type: 'Whatever' })).toEqual(state)
+    it('shows load modal on Tournament/ShowLoadModal', () => {
+        expect(TournamentReducer(state, ShowLoadModal())).toEqual(
+            {
+                name: 'Test Tournament',
+                loading: false,
+                saveModal: false,
+                loadModal: true
+            }
+        )
+    })
+
+    it('shows save modal on Tournament/ShowSaveModal', () => {
+        expect(TournamentReducer(state, ShowSaveModal())).toEqual(
+            {
+                name: 'Test Tournament',
+                loading: false,
+                saveModal: true,
+                loadModal: false
+            }
+        )
+    })
+
+    it('hides load modal on Tournament/HideLoadModal', () => {
+        expect(TournamentReducer({...state, loadModal: true}, HideLoadModal())).toEqual(
+            {
+                name: 'Test Tournament',
+                loading: false,
+                saveModal: false,
+                loadModal: false
+            }
+        )
+    })
+
+    it('hides save modal on Tournament/HideSaveModal', () => {
+        expect(TournamentReducer({...state, saveModal: true}, HideSaveModal())).toEqual(
+            {
+                name: 'Test Tournament',
+                loading: false,
+                saveModal: false,
+                loadModal: false
+            }
+        )
+    })
+
+    it('hides both modals on Tournament/HideModals', () => {
+        expect(TournamentReducer({...state, saveModal: true}, HideTournamentModals())).toEqual(
+            {
+                name: 'Test Tournament',
+                loading: false,
+                saveModal: false,
+                loadModal: false
+            }
+        )
+
+        expect(TournamentReducer({...state, loadModal: true}, HideTournamentModals())).toEqual(
+            {
+                name: 'Test Tournament',
+                loading: false,
+                saveModal: false,
+                loadModal: false
+            }
+        )
     })
 })
--- src/Tournament/Reducers/TournamentReducer.js
+++ src/Tournament/Reducers/TournamentReducer.js
@@ -1,7 +1,34 @@
 import InitialState from '../../Shared/InitialState'
+import {Types} from '../../Shared/Actions'
 
 export default (state = InitialState.tournament, action) => {
     switch (action.type) {
+        case Types.TOURNAMENT_SHOW_SAVE_MODAL:
+            return {
+                ...state,
+                saveModal: true
+            }
+        case Types.TOURNAMENT_SHOW_LOAD_MODAL:
+            return {
+                ...state,
+                loadModal: true
+            }
+        case Types.TOURNAMENT_HIDE_SAVE_MODAL:
+            return {
+                ...state,
+                saveModal: false
+            }
+        case Types.TOURNAMENT_HIDE_LOAD_MODAL:
+            return {
+                ...state,
+                loadModal: false
+            }
+        case Types.TOURNAMENT_HIDE_MODALS:
+            return {
+                ...state,
+                loadModal: false,
+                saveModal: false
+            }
         default:
             return state
     }
